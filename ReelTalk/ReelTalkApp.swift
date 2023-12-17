b96ea22b8fa1807fbaa594b41b02706f1fe187c5
LuiArm <Luisarmendariz24@icloud.com>
2023-12-17 12:13:15 -0700

    Initial Commit

---
+++ ReelTalk/ReelTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelTalkApp.swift
+//  ReelTalk
+//
+//  Created by luis armendariz on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct ReelTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

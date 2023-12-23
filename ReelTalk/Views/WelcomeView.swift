8fcc80a59103ab72134c065754d66bde80378d26
LuiArm <Luisarmendariz24@icloud.com>
2023-12-22 23:24:09 -0700

    Carousel ui start in welcomeview

---
+++ ReelTalk/Views/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  ReelTalk
+//
+//  Created by luis armendariz on 12/22/23.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}

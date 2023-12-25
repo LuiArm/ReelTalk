d2f760fd3ad31f96179f5961edd147ff8fbfeaf1
LuiArm <Luisarmendariz24@icloud.com>
2023-12-24 17:44:56 -0700

    Welcome view, assets

--- ReelTalk/ContentView.swift
+++ ReelTalk/ContentView.swift
@@ -8,25 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var index = 0
+    
+    
     var body: some View {
-        VStack {
-            TabView(selection: $index){
-                ForEach((0...3), id: \.self){ index in
-                    WelcomeView()
-                }
-            }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-        }
-        HStack(spacing: 2) {
-            ForEach((0...3), id: \.self) { index in
-                Rectangle()
-                    .fill(index == self.index ? Color.purple : Color.purple.opacity(0.5))
-                    .frame(width: 30, height: 5)
-
-            }
-        }
-        .padding()
+       WelcomeView()
+    
     }
 }
 
--- ReelTalk/Views/WelcomeView.swift
+++ ReelTalk/Views/WelcomeView.swift
@@ -8,15 +8,60 @@
 import SwiftUI
 
 struct WelcomeView: View {
+    private let welcomeImages = ["Welcome1", "Welcome2", "Welcome3"]
+    
+    @State private var index = 0
+    
     var body: some View {
-        Rectangle()
-            .fill(Color.pink)
-            
-            .border(Color.black)
-            .padding()
+        ZStack{
+            ScrollView(.horizontal) {
+                    LazyHStack(spacing: 0) {
+                        ForEach(welcomeImages, id: \.self) { index in
+
+                            Image(index)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(height: 250)
+                                .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                                .padding(.horizontal, 20)
+                                .containerRelativeFrame(.horizontal)
+                        }
+                    }
+                    .scrollTargetLayout()
+                }
+            .scrollTargetBehavior(.viewAligned)
+        }
+        .background(Color.black)
+        
+        
+        
+        
+        
+//        VStack {
+//            TabView(selection: $index){
+//                ForEach(welcomeImages, id: \.self){ image in
+//                    Image(image)
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(height: 450)
+//                }
+//            }
+//            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+//        }
+//        HStack(spacing: 2) {
+//            ForEach((0..<3), id: \.self) { index in
+//                Rectangle()
+//                    .fill(index == self.index ? Color.purple : Color.purple.opacity(0.5))
+//                    .frame(width: 30, height: 5)
+//
+//            }
+//        }
+//        .padding()
     }
 }
 
 #Preview {
     WelcomeView()
 }
+
+
